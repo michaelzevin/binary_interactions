9e34352adb15953998073ed092a16e5443372f1f
Michael Zevin <mzevin1@gmail.com>
2017-11-09 16:37:20 -0600

    removed auxiliary files that need to be generated on a specific machine

--- main_files/gslwr.c
+++
@@ -1,4 +0,0 @@
-#include <gsl/gsl_sf_ellint.h>
-void c_wrapper_gsl_sf_ellint_rj_(double* R, double* x, double* y, double* z, double* p, double* err){
-        *R = gsl_sf_ellint_RJ(*x, *y, *z, *p, *err);
-}
